9f2809e089867ddd319f00e1878325ccf13b01e6
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-06-30 21:45:38 +0700

    8. Reading and Writing Files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
-const hello = 'Hello World!';
-console.log(hello);
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
 
-
+const textOut = `This is something: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File has been written.');
