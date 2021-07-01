a195436c22dd070421c3cf08eebf1a90650b2b1b
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-01 23:37:10 +0700

    12. Routing

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    console.log(res);
-    res.end('Hello from the server!');
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is overview.');
+    } else if (pathName === '/product') {
+        res.end('This is product.');
+    } else {
+        res.writeHead(404, { 
+            'Content-Type': 'text/html',
+            'my-custom-header': 'anything-for-value'
+        });
+        res.end('<h1>Route not found.</h1>');
+    }
 });
  
 server.listen(8000, 'localhost', () => {
