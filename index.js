6bcceaff25628e58aa701365d2a760e31a8795b8
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-01 23:14:16 +0700

    11. Creating a Simple Web Server

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-const fs = require('fs');
+const http = require('http');
 
-// Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
-
-// const textOut = `This is something: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('File has been written.');
-
-// Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
-
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written!');
-            });
-        });
-    });
+const server = http.createServer((req, res) => {
+    console.log(res);
+    res.end('Hello from the server!');
 });
-
-console.log('Reading the file');
+ 
+server.listen(8000, 'localhost', () => {
+    console.log('Listening on port 8000')
+});
 
