812d3bc35f57974c3bf1c14c8001e312c7ef304f
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-10 22:52:21 +0700

    13. Building a (Very) Simple API

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
@@ -9,6 +12,11 @@ const server = http.createServer((req, res) => {
         res.end('This is overview.');
     } else if (pathName === '/product') {
         res.end('This is product.');
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 
+            'Content-Type': 'application/json'
+        });
+        res.end(data);        
     } else {
         res.writeHead(404, { 
             'Content-Type': 'text/html',
