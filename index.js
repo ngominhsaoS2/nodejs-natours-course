4028137b44dffd801702719a1ffe62250ace1df9
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-10 23:29:12 +0700

    14. HTML Templating: Building the Templates

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const url = require('url');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
