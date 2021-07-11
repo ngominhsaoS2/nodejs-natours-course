489e2ae7463a7a9b50916ad44f140580bd1246c4
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-07-11 10:17:15 +0700

    16. Parsing Variables from URLs

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+
+    const { query, pathname } = url.parse(req.url, true);
 
     // Overview Page
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 
             'Content-Type': 'text/html',
         });
@@ -42,11 +43,19 @@ const server = http.createServer((req, res) => {
         res.end(output);
 
     // Product Page
-    } else if (pathName === '/product') {
-        res.end('This is product.');
+    } else if (pathname === '/product') {
+        //console.log(query);
+
+        res.writeHead(200, { 
+            'Content-Type': 'text/html',
+        });
+
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
 
     // Api
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
         res.writeHead(200, { 
             'Content-Type': 'application/json'
         });
